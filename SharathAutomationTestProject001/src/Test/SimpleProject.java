85eb16e1833a0a62cc2b8a0bd91875cade321fe4
sharathcm17 <84670426+sharathcm17@users.noreply.github.com>
2021-05-27 19:39:12 +0530

    6th commit

--- SharathAutomationTestProject001/src/Test/SimpleProject.java
+++ SharathAutomationTestProject001/src/Test/SimpleProject.java
@@ -1,41 +1,58 @@
 package Test;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import com.google.common.io.Files;
+
 
 public class SimpleProject {
 
-WebDriver driver;
-	
+	WebDriver driver;
+
+
 	@BeforeTest
 	public void setup(){
-		
-		System.out.println("Execution started");
+		Reporter.log("Setting up the browser");
 		System.setProperty("webdriver.chrome.driver", "..\\SharathAutomationTestProject001\\drivers\\chromedriver.exe");
-		
+
 		driver = new ChromeDriver();
-		
+		Reporter.log("Setting up done");
+
 	}
-	
+
 	@Test
-	public void execute() throws InterruptedException {
+	public void execute() throws InterruptedException, IOException {
 		
-	driver.get("https://www.google.com/");
-	Thread.sleep(12000);
+		Reporter.log("Opening the link Google");
+		driver.get("https://www.google.com/");
+		Reporter.log("Link got Opened and waiting for 12 sec");
+		Thread.sleep(12000);
+		Reporter.log("Wait over");
+			
+			
+		}
+
 	
-	}
-	
-	
+
+
 	@AfterTest
 	public void tearDown() {
+
+		Reporter.log("Quitting the browser");
+		driver.quit();
+		Reporter.log("browser window is closed now");
 		
-	driver.quit();
-	
-	System.out.println("execution completed and test case passed");
-}
-	
+	}
+
 
 }
 
